991b09be4e51eb7ee1e6ffaabda1163628ef5e4c
Hank Wang <drapho@gmail.com>
2020-09-16 14:32:06 +0900

    create sample app

---
+++ SampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Hank Wang on 2020/09/16.
+//  Copyright © 2020 Hank Wang. All rights reserved.
+//
+
+import UIKit
+import SampleiOSKit
+
+class ViewController: SampleViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        changeBackground(withColor: .blue)
+    }
+}
+
