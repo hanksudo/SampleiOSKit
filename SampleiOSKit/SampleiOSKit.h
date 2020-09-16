1c98993b705caa44dd298569394ecf7ac23da9d1
Hank Wang <drapho@gmail.com>
2020-09-16 14:25:09 +0900

    init commit

---
+++ SampleiOSKit/SampleiOSKit.h
@@ -0,0 +1,19 @@
+//
+//  SampleiOSKit.h
+//  SampleiOSKit
+//
+//  Created by Hank Wang on 2020/09/16.
+//  Copyright © 2020 Hank Wang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SampleiOSKit.
+FOUNDATION_EXPORT double SampleiOSKitVersionNumber;
+
+//! Project version string for SampleiOSKit.
+FOUNDATION_EXPORT const unsigned char SampleiOSKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleiOSKit/PublicHeader.h>
+
+
