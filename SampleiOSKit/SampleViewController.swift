2132574b38c672b851bab1efff0c2b94866b4179
Hank Wang <drapho@gmail.com>
2020-09-16 14:31:57 +0900

    create SampleViewController

---
+++ SampleiOSKit/SampleViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SampleViewController.swift
+//  SampleiOSKit
+//
+//  Created by Hank Wang on 2020/09/16.
+//  Copyright © 2020 Hank Wang. All rights reserved.
+//
+
+import UIKit
+
+open class SampleViewController: UIViewController {
+    public func changeBackground(withColor color: UIColor) {
+        view.backgroundColor = color
+    }
+}
